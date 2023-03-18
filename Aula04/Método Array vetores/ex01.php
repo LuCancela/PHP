7ee3e70924f8fc4a55de8af701a283e7c283e59e
LuCancela <87439428+LuCancela@users.noreply.github.com>
2023-03-17 22:24:54 -0300

    Vetores

---
+++ Aula04/Método Array vetores/ex01.php
@@ -0,0 +1,26 @@
+<?php
+$vetor= Array();
+
+$vetor [0]=12;
+$vetor [1]=21;
+$vetor [2]= 22;
+$vetor [3]=48;
+$vetor [4]=57;
+$vetor [5]=98;
+$vetor [6]= 101;
+$vetor [7]= 202;
+$vetor [8]=201;
+$vetor [9]= 312;
+
+ for($i=0;$i<10;$i++){
+    
+    {
+        if($vetor [$i]  %2 ==0){
+            echo $vetor [$i] . '<br>';
+    }
+    
+
+    }
+ }
+
+?>
---
+++ Aula04/index.php
@@ -0,0 +1,11 @@
+<?php
+$vetor= Array();
+
+$vetor [1]=12;
+
+echo $vetor[1];
+
+
+
+
+?>
