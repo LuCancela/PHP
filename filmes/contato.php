7175537d443aa1c7666b0c300bae1ca1691f0d9a
LuCancela <cancelalucas@gmail.com>
2023-04-28 22:19:13 -0300

    Projeto filmes

---
+++ filmes/contato.php
@@ -0,0 +1,9 @@
+<form action="contato-salvar.php" method="POST">
+
+    <input type="text" placeholder="nome" name="nome">
+    <input type="text" placeholder="email" name="email">
+    <input type="text" placeholder="assunto" name="assunto">
+    <input type="text" placeholder="mensagem" name="mensagem">
+    <input type="submit" value="Salvar">
+
+</form>
---
+++ filmes/index.php
@@ -0,0 +1,52 @@
+<?php include("cabecalho.php"); ?>
+
+
+
+<div class="banner">
+    <img src="images/banner.png" alt="">
+    <div class="preto"></div>
+</div>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img class="d-block w-100" src="pearl.png" alt="First slide">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>O MARIO É FODA PIPIPOPO</h5>
+                <p>O MARIO É FODA PIPIPOPO</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="images/dragons.png" alt="Second slide">
+            <h5>....</h5>
+            <p>...</p>
+        </div>
+        <div class="carousel-item">
+            <img class="d-block w-100" src="images/john.png" alt="Third slide">
+            <h5>....</h5>
+            <p>...</p>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+
+</div>
+
+
+<p>DESCRIÇÃO DO NOSSO SITE:</p>
+<p>Aqui teremos filmes e generos:</p>
+<p>Basicamente, se você precisa procurar algum filme, ver sua sinopse ou coisas do tipo, o BLL Filmes é o seu site, aqui
+    vamos dar dicas e descrições para facilitar sua noite de maratonas.
+</p>
+< <?php include("rodape.php"); ?>
